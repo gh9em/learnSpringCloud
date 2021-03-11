385fe3a98dff9b189a2290213395f74abba62ffc
gh9em <gh_9em@9em.org>
2021-03-11 08:34:04 +0000

    fix(cloud-provider-payment8001): replace @Controller with @RestController

--- cloud-project/cloud-provider-payment8001/src/main/java/com/learn/springcloud/controller/PaymentController.java
+++ cloud-project/cloud-provider-payment8001/src/main/java/com/learn/springcloud/controller/PaymentController.java
@@ -5,15 +5,15 @@ import com.learn.springcloud.entities.Payment;
 import com.learn.springcloud.service.PaymentService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Controller
+@RestController
 public class PaymentController {
     @Autowired
     private PaymentService paymentService;
