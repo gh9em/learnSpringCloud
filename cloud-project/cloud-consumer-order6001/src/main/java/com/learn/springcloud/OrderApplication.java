994a443e386afe4cc1da611ee1fa10724cc3ea2d
gh9em <gh_9em@9em.org>
2021-03-13 11:00:54 +0000

    feat(cloud-consumer-order6001): add custom ribbon load balance rule

--- cloud-project/cloud-consumer-order6001/src/main/java/com/learn/springcloud/OrderApplication.java
+++ cloud-project/cloud-consumer-order6001/src/main/java/com/learn/springcloud/OrderApplication.java
@@ -1,11 +1,15 @@
 package com.learn.springcloud;
 
+import com.learn.rule.RibbonRule;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = RibbonRule.class)
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
